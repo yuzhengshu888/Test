849d17977cea2bc4da876af55b0912b3014041d2
yuzhengshu <zhengyu.yu@myt11.com>
2021-02-19 15:26:43 +0800

    测试提交

--- Hi_demo/src/com/yzs/test.java
+++ Hi_demo/src/com/yzs/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         demo1 demo1=new demo1();
         demo1.init();
-
+//测试
         //demo1.addclass(2,"软件2班");
 
        // demo1.addstudent(1001,"老四","男",2,"2019-4-26");
